3d2c124b8478da77924df5955b587bad7bf5290f
Kay Lima <kay@inn.org>
2018-02-13 09:22:11 -0700

    updating function name to mstoday, remove commenting function and make sure post-tags.php is in /inc

--- wp-content/themes/mstoday/inc/post-tags.php
+++ wp-content/themes/mstoday/inc/post-tags.php
@@ -7,10 +7,12 @@
  *
  * The 'brute' in this function name is because it uses string splicing and addition functions to add a button.
  *
- * @link http://jira.inn.org/browse/NQ-127A
+ * @link https://secure.helpscout.net/conversation/489905533/1575
+ * @link https://github.com/INN/umbrella-mstoday/issues/4
+ * @link https://github.com/INN/umbrella-rivard-report/blob/master/wp-content/themes/rivard-report/inc/post-tags.php#L6-L44 copied from here
  * @param string $input the post social links HTML.
  */
-function rr_post_social_brute_add_linkedin( $input ) {
+function mstoday_post_social_brute_add_linkedin( $input ) {
 	// The start of the "More social links" menu.
 	$before = '<span class="more-social-links';
 	// Get the postition of the end of $before.
@@ -28,11 +30,8 @@ function rr_post_social_brute_add_linkedin( $input ) {
 		'<span class="linkedin"><a href="%s" target="_blank"><i class="icon-linkedin"></i> <span class="hidden-phone">Share <span class="visuallyhidden">on LinkedIn</span></span></a></span>',
 		$link
 	);
-	// If you link to #disqus_thread, the obvious thing to do, Disqus will replace the inner html of the first instance of that on the page with a comment count.
-	// @see rr_fake_comments_link
-	$string .= '<span class="comments"><a href="#comments_link"><i class="icon-comment"></i> <span class="hidden-phone">Comments </span></a></span>';
 	// Add the button to the largo_post_social_links HTML.
 	$output = substr_replace( $input, $string, $position, 0 );
 	return $output;
 }
-add_filter( 'largo_post_social_links', 'rr_post_social_brute_add_linkedin' );
+add_filter( 'largo_post_social_links', 'mstoday_post_social_brute_add_linkedin' );
