4a509e823aef2b75a63c335f9c32d8eaa7b17ac7
Ben Keith <ben@benlk.com>
2018-06-20 19:03:46 -0400

    bug fix: output <meta name="description"> tags using largo_opengraph logic, because Yoast SEO doesn't.
    This continues the work in https://github.com/INN/umbrella-mstoday/pull/13,
    addressing specifically the fact that the Yoast SEO plugin outputs
    twitter:description and og:description tags, but not plain description tags.
    
    So here we copy a bunch of logic from largo_opengraph, and run it
    if Yoast is active.

--- wp-content/themes/mstoday/inc/open-graph.php
+++ wp-content/themes/mstoday/inc/open-graph.php
@@ -4,7 +4,12 @@
  */
 
 /**
- * Zero out the OpenGraph tags produced by Largo if Yoast is active
+ * Zero out the OpenGraph tags produced by Largo if Yoast is active,
+ *
+ * but don't hide the meta description tag, because that's apparently something
+ * that Yoast SEO does not output. Why, I don't know. Anyhow, copy a bunch of
+ * logic from largo_opengraph(); and run it if Yoast is running, just for the
+ * meta name="description" tag.
  *
  * @link https://github.com/INN/largo/issues/1437
  * @link https://wordpress.org/plugins/wordpress-seo/
@@ -16,6 +21,39 @@ function mstoday_opengraph() {
 	// this is defined in wordpress-seo/wp-seo-main.php.
 	if ( ! defined( 'WPSEO_VERSION' ) ) {
 		largo_opengraph();
+	} else {
+		// Yoast does not take care of these tags. :frown:
+		if ( is_single() ) {
+			if ( have_posts() ) {
+				the_post(); // we need to queue up the post to get the post specific info
+				?>
+					<meta name="description" content="<?php echo strip_tags( esc_html( get_the_excerpt() ) ); ?>" />
+				<?php
+			}
+			rewind_posts();
+		} elseif ( is_home() ) {
+			?>
+				<meta name="description" content="<?php bloginfo( 'description' ); ?>" />
+			<?php
+		} else {
+			// let's try to get a better description when available.
+			if ( is_category() && category_description() ) {
+				$description = category_description();
+			} elseif ( is_author() ) {
+				if ( have_posts() ) {
+					the_post(); // we need to queue up the post to get the post specific info.
+					if ( get_the_author_meta( 'description' ) ) {
+						$description = get_the_author_meta( 'description' );
+					}
+				}
+				rewind_posts();
+			} else {
+				$description = get_bloginfo( 'description' );
+			}
+			if ( $description ) {
+				echo '<meta name="description" content="' . strip_tags( esc_html( $description ) ) . '" />';
+			}
+		}
 	}
 }
 
