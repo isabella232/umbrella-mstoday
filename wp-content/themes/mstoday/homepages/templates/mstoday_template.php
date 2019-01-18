fba75d2551e17b8a4dc0f92f1766fbc28b56bd99
tylermachado <tyler@inn.org>
2019-01-18 17:31:28 -0500

    reconfiguring template areas

--- wp-content/themes/mstoday/homepages/templates/mstoday_template.php
+++ wp-content/themes/mstoday/homepages/templates/mstoday_template.php
@@ -1,21 +1,19 @@
 <?php global $shown_ids; ?>
 
-<div class="homepage-top-post">
+<div class="homepage-top-rail-right span4">
+	<?php echo $homepage_featured_projects_widget; ?>
+</div>
+<div class="homepage-top-post span8">
 	<?php echo $homepage_top_story; ?>
 </div>
-<div class="clearfix"></div>
-<div class="homepage-top-rail">
 
+<div class="clearfix"></div>
+
+<div class="homepage-top-rail">
 	<div class="homepage-top-rail-left">
 		<?php echo $homepage_second_story; ?>
 	</div>
-
-	<div class="homepage-top-rail-right">
-		<?php echo $homepage_featured_projects_widget; ?>
-	</div>
-	
 	<div class="clearfix"></div>
-
 </div>
 
 <div class="homepage-river">
