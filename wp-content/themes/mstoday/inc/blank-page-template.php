2f251d335cb5c150de8cbb56b2b5527cc003053d
Ben Keith <ben@benlk.com>
2019-04-11 18:08:34 -0400

    Better way of hiding the main nav and only showing the sticky nav: Change the option output.

--- wp-content/themes/mstoday/inc/blank-page-template.php
+++ wp-content/themes/mstoday/inc/blank-page-template.php
@@ -17,17 +17,49 @@ function mstoday_blank_page_largo_floating_social_buttons() {
 /**
  * Override Largo's enqueue options for sticky nav
  *
+ * This is a filter on the "option_{$option}" filter, which allows
+ * us to change the value of a `get_option( $option )` call, before
+ * that value is returned.
+ *
+ * Using this filter, we modify the result of get_option calls when
+ * this filter is active.
+ *
+ * This should only be enqueued on single-blank.php page loads.
+ *
  * @see single-blank.php
+ * @since Largo 0.6.1
+ * @param Mixed $value The value returned
+ * @param String $option the option name
  */
-function mstoday_blank_page_largo_enqueue_js() {
-	wp_localize_script(
-		'largoCore', 'Largo', array(
-		'is_home' => is_front_page(),
-		'is_single' => is_single() || is_singular(),
-		'sticky_nav_options' => array(
-			'sticky_nav_display' => true,
-			'main_nav_hide_article' => true,
-			'nav_overflow_label' => of_get_option( 'nav_overflow_label', 'More' )
-		)
-	));
+function mstoday_blank_page_options_filter( $value, $option ) {
+	if ( is_array( $value ) ) {
+		$value['main_nav_hide_article'] = '1';
+		$value['sticky_nav_display'] = '1';
+	}
+	error_log(var_export( $value, true));
+	return $value;
+}
+
+/**
+ * Enqueue mstoday_blank_page_options_filter using the correct ID.
+ *
+ * This is necessary because we must make sure that the
+ * "option_{$option}" filter has the correct $option.
+ *
+ * It uses the same logic as the function of_get_option, the return
+ * value for which we are attempting to modify.
+ *
+ * @link https://github.com/INN/largo/blob/099fd2c9361b7a4e9e6e729d35adde40685cb0fb/lib/options-framework/options-framework.php#L410
+ * @see mstoday_blank_page_options_filter
+ * @since Largo 0.6.1
+ */
+function mstoday_blank_page_options_filter_register() {
+	$config = get_option( 'optionsframework' );
+	if ( ! isset( $config['id'] ) ) {
+		error_log(var_export( 'huh', true));
+		return false;
+	}
+	$filter = 'option_' . $config['id'];
+
+	add_filter( $filter, 'mstoday_blank_page_options_filter', 10, 2 );
 }
